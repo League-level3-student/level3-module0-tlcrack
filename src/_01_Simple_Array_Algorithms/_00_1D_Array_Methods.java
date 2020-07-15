c6a55037c55c4222dbe8eb00cb5777a46ecc2db3
root <root@c30f6c74781c>
2020-07-15 01:26:31 +0000

    Finished level 3, module 0

--- src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
+++ src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
@@ -4,15 +4,22 @@ public class _00_1D_Array_Methods {
 	//1. Complete the method so that it returns the sum of all
 	//   of the integers in the array being passed in
 	public static int sumIntArray(int[] values) {
-
-		return 0;
+		int valueSum = 0;
+		for(int i = 0; i <= values.length-1; i++) {
+			valueSum+=values[i];
+		}
+		return valueSum;
 	}
 	
 	//2. Complete the method so that it returns the average of all
 	//   of the integers in the array being passed in
 	public static double averageIntArray(int[] values) {
-				
-		return 0;
+		double valueSum = 0.0;
+		double valueAverage = 0.0;
+		for(int i = 0; i <= values.length-1; i++) {
+			valueSum+=values[i];}	
+		valueAverage+=(valueSum/values.length);
+		return valueAverage;
 	}
 	
 	
@@ -20,7 +27,13 @@ public class _00_1D_Array_Methods {
 	//   array contains the value specified by the second parameter.
 	//   It should otherwise return false.
 	public static boolean containsIntValue(int[] array, int value) {
-	
+		for(int i = 0; i <= array.length-1; i++) {
+			if(array[i]==value) {
+				return true;
+			}
+		}
+		
+		
 		return false;
 	}
 	
@@ -28,7 +41,11 @@ public class _00_1D_Array_Methods {
 	//   first instance that the specified value occurs in the array.
 	//   If the array does not contain the specified value, it should return -1.
 	public static int getIndex(int[] arr, int value) {
-		
-		return 0;
+		for(int i = 0; i <= arr.length-1; i++) {
+			if(arr[i]==value) {
+				return i;
+			}
+		}
+		return -1;
 	}
 }
--- src/_03_More_Array_Fun/MoreArrayFun.java
+++ src/_03_More_Array_Fun/MoreArrayFun.java
@@ -1,26 +1,67 @@
 package _03_More_Array_Fun;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class MoreArrayFun {
-	//1. Create a main method to test the other methods you write.
-	
+	static //1. Create a main method to test the other methods you write.
+	Random random = new Random();
+	public static void main(String[] args) {
+		String[] strings = new String[]{"testing, ", "testing, ", "1, ", "2, ", "3"};
+		stringPrinter(strings);
+		backwardsStringPrinter(strings);
+		halfStringPrinter(strings);
+		randomStringPrinter(strings);
+	}
 	
 	
 	//2. Write a method that takes an array of Strings and prints all the Strings in the array.
-	
+	static void stringPrinter(String[] array) {
+		for(int i = 0; i <= array.length-1; i++) {
+			System.out.println(array[i]);
+		}
+	}
 	
 	
 	//3. Write a method that takes an array of Strings and prints all the Strings in the array
 	//   in reverse order.
+	static void backwardsStringPrinter(String[] array) {
+		for(int i = array.length-1; i >= 0; i--) {
+		System.out.println(array[i]);
+		}
 	
-	
-	
+	}
 	//4. Write a method that takes an array of Strings and prints every other String in the array.
-	
+	static void halfStringPrinter(String[] array) {
+		for(int i = 0; i <= array.length-1; i++) {
+			if(i%2==0) {
+			System.out.println(array[i]);
+			}
+		}
+	}
 	
 	//5. Write a method that takes an array of Strings and prints all the Strings in the array
 	//   in a completely random order. Almost every run of the program should result in a different order.
-	
+	static void randomStringPrinter(String[] array) {
+		ArrayList<Integer> usedIndexes = new ArrayList<>();
+		
+		System.out.println("--------------------------------");
+		
+		for(int i = 0; i <= array.length-1; i++) {
+			// 1. Get random number
+			int r = random.nextInt(array.length);
+			
+			// 2. Check if that number has already been printed
+			if( usedIndexes.contains(r) ) {
+				// Number has been printed
+				i--;
+				continue;
+			} else {
+				// Number has NOT been printed so print it
+				usedIndexes.add(r);
+				System.out.println(array[r]);
+			}
+		}
+	}
 	
 }
